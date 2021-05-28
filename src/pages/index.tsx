110a686cc633eb0ba9e557a437568f651d32163d
NatanTavares <nattantavares.s15@gmail.com>
2021-05-27 22:43:19 -0300

    feat(sign-in): Implements the visual part of the Sign-In screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,62 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+
 export default function Home() {
-  return <h1>Dash.go</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        direction="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              id="email"
+              size="lg"
+              variant="filled"
+              bg="gray.900"
+              focusBorderColor="pink.500"
+              _hover={{
+                bg: "gray.900",
+              }}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              size="lg"
+              variant="filled"
+              bg="gray.900"
+              focusBorderColor="pink.500"
+              _hover={{
+                bg: "gray.900",
+              }}
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" size="lg" mt="6" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
