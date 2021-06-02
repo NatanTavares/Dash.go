02a70738e158814c0368bfba1fc2b3e4401d21e8
NatanTavares <nattantavares.s15@gmail.com>
2021-06-02 20:19:22 -0300

    feat(users): Applies responsivity

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -21,7 +21,7 @@ export default function CreateUser() {
       <Flex w="100%" maxW={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">
             Create user
           </Heading>
@@ -29,12 +29,12 @@ export default function CreateUser() {
           <Divider my="6" borderColor="gray.700" />
 
           <Stack spacing="8">
-            <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+            <SimpleGrid minChildWidth={240} spacing={["6", "8"]} w="100%">
               <Input name="name" label="Full name" />
               <Input name="email" label="E-mail" type="email" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+            <SimpleGrid minChildWidth={240} spacing={["6", "8"]} w="100%">
               <Input name="password" label="Password" type="password" />
               <Input
                 name="password_confirmation"
